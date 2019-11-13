f62dea0ab9bdbd08bee6179a4cff36b20f3e546f
Annie Zheng <anniezheng.anniee@gmail.com>
2019-11-13 01:24:06 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -10,7 +10,7 @@ def directors_totals(nds)
   dir_index = 0 
   while dir_index < nds.length do 
     dir = nds[dir_index]
-    result[dir[:name]] += gross_for_director(dir)
+    result[dir[:name]] = gross_for_director(dir)
     dir_index += 1 
   end 
   result
